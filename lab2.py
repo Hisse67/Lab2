cd1e5c9763f70efcc9453bd4b0e078dc098bd970
Hisse67 <156141447+Hisse67@users.noreply.github.com>
2024-02-12 20:38:36 -0500

    lab2.py

---
+++ lab2.py
@@ -0,0 +1,12 @@
+import sys 
+
+name = str(sys.argv[1])
+age = int(sys.argv[2])
+
+print("Hi " + name +, you are " + str(age) + " years old.")
+print("Variable1 =", name)
+print("Variable2 =", age)
+
+print("The script and variables used are:")
+print("Script:lab2.py")
+print("Variables: name =", name, ", age =", age)
